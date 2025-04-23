134691f3b0b58a36bcca9e94d2da8a43d0ac571c
AkinOmisakin <u04ao21@abdn.ac.uk>
2025-04-24 00:11:50 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.chaquo.python")
+}
+
+android {
+    namespace = "com.frontend.medapp"
+    compileSdk = 35
+
+    defaultConfig {
+
+        applicationId = "com.frontend.medapp"
+        minSdk = 30 // at least 24
+        targetSdk = 35 //stable release
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ndk {
+            // specify the ABI filters for the native libraries
+            abiFilters += listOf("arm64-v8a", "x86_64", "armeabi-v7a", "x86")
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+//chaquopy block
+chaquopy {
+    defaultConfig {
+        version = "3.11"
+
+        pip {
+            install("-r", "requirements.txt")
+        }
+        pyc {
+            src = true
+        }
+
+    }
+}
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
---
+++ app/src/main/python/backend/client/FLClient.py
@@ -0,0 +1,21 @@
+import flwr
+from flwr.client import Client, ClientApp, NumPyClient
+from flwr.common import (
+    Metrics,
+    Context,
+    Code,
+    EvaluateIns,
+    EvaluateRes,
+    FitIns,
+    FitRes,
+    GetParametersIns,
+    GetParametersRes,
+    Status,
+    ndarrays_to_parameters,
+    parameters_to_ndarrays,
+)
+from flwr.server import ServerApp, ServerConfig, ServerAppComponents
+from flwr.server.strategy import FedAvg
+from flwr.simulation import run_simulation
+from flwr_datasets import FederatedDataset
+
